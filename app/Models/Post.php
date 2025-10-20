2ab48359cd8c9b7fd69e04fe94e347b328329bbc
khaled.ar <khaled.ar.work@gmail.com>
2025-10-20 02:35:00 -0700

    category

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -87,9 +87,4 @@ class Post extends Model
     {
         return $this->belongsTo(User::class)->with(['employee', 'general_user']);
     }
-
-    public function category()
-    {
-        return $this->belongsTo(Category::class);
-    }
 }
